7d8fb51d685b69f63385678d35e892933e933b62
narasaki ryosei <gennarikun@gmail.com>
2023-03-31 01:23:59 +0900

    add:アコーディオン機能

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
 ((d, w) => {
-  
+  /**
+    アコーディオン
+  */
+  const headline = d.querySelectorAll('.js-accordion_headline');
+  const accordionItem = d.querySelectorAll('.js-accordion_item');
+  for (let i = 0; i < headline.length; i++) {
+    headline[i].addEventListener('click', (e) => {
+      e.target.parentNode.classList.toggle('is-accordion_item');
+    });
+  }
 })(document, window);
