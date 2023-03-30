f7d6eb140c0137449c4860ccd40929c148dfa762
narasaki ryosei <gennarikun@gmail.com>
2023-03-31 01:26:55 +0900

    fix:jsリファクタリング

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@
     アコーディオン
   */
   const headline = d.querySelectorAll('.js-accordion_headline');
-  const accordionItem = d.querySelectorAll('.js-accordion_item');
   for (let i = 0; i < headline.length; i++) {
     headline[i].addEventListener('click', (e) => {
       e.target.parentNode.classList.toggle('is-accordion_item');
